d5894f199384ed0fe75fb55f116186d45e54d2b5
AdawaleShubham <adawale8334@gmail.com>
2022-06-29 19:26:53 +0530

    hero info

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let btnClear = document.querySelector('btn-2');
-let inputs = document.querySelectorAll('input');
-btnClear.addEventListener('click', () => {
-    inputs.forEach(input =>  input.value = '');
-});
+// let btnClear = document.getElementById('button');
+// let inputs = document.querySelectorAll('input');
+
+// btnClear.addEventListener('click', () => {
+//     inputs.forEach(input =>  input.value = '');
+// });
+// ,email,phone,subject,message
